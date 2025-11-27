b4785ed84d33f3c7490f8b842aad0bc45bdc4be1
MatanH5 <matan.hoffert@gmail.com>
2025-11-27 13:49:58 +0200

    Update Encryption.java
    mulis' code

--- Encryption.java
+++ Encryption.java
@@ -6,12 +6,32 @@
  */
 public class Encryption
 {
+    // muli
+    public static String replace(String input)// replace from words to signs
+    {    
+        String str = input;
+        str = str.replace('o','0' );
+        str = str.replace('a','@' );
+        str = str.replace('e','#' );
+        str = str.replace('i','1' );
+        str = str.replace('u','&' );
+        return str;
+    }
+   // muli
+    public static String restore (String sentence){ // replace from signs to words
+        String str = sentence;
+        str = str.replace('0','o' );
+        str = str.replace('@','a' );
+        str = str.replace('#','e' );
+        str = str.replace('1','i' );
+        str = str.replace('&','u' );
+        return str;
+    }
+    
     public static void main(String[] args){
         
         System.out.println("Welcome to the Encryption / Decryption Program");
         
     }
-    public static int add (int a, int b){
-        return a+b;
     }
 }
