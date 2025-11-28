e91b1b39b62666e0c4d64cdf2fae3b03c7d6d195
muli122123 <kofall670@gmail.com>
2025-11-28 10:35:37 +0200

    Update EncryptionTester.java
    5 tessters of muli's code
    (:    🥇

--- EncryptionTester.java
+++ EncryptionTester.java
@@ -54,18 +54,76 @@ public class EncryptionTester
         assertEquals("ccc aaa bbb",result);
         
     }
-     @Test
-    public void testAdd6(){//muli
-        
-        String result = Encryption.replace("abuigo");
-        assertEquals("@b&1g0",result);
-        
-    }
+     
     @Test
-    public void testAdd7(){//muli
+    public void testAdd6(){//muli
         
         String result = Encryption.restore(""@b&1g0");
         assertEquals("abuigo",result);
         
     }
+     @Test
+    public void testAdd7(){//muli
+        
+        String result = Encryption.restore(""@&10");
+        assertEquals("auio",result);
+        
+    }
+     @Test
+    public void testAdd8(){//muli
+        
+        String result = Encryption.restore(""00 11 &&@@");
+        assertEquals("oo ii uuaa",result);
+        
+    }
+     @Test
+    public void testAdd9(){//muli
+        
+        String result = Encryption.restore(""00 @@ p0");
+        assertEquals("oo aa po",result);
+        
+    }
+     @Test
+    public void testAdd10(){//muli
+        
+        String result = Encryption.restore(""o01i@");
+        assertEquals("ooiia",result);
+        
+    }
+        @Test
+    public void testAdd11(){//muli
+        
+        String result = Encryption.replace("abuigo");
+        assertEquals("@b&1g0",result);
+        
+    }
+        @Test
+    public void testAdd12(){//muli
+        
+        String result = Encryption.replace("aa bb io");
+        assertEquals("@@ bb 10",result);
+        
+    }
+        @Test
+    public void testAdd13(){//muli
+        
+        String result = Encryption.replace("abcs");
+        assertEquals("@bcs",result);
+        
+    }
+        @Test
+    public void testAdd14(){//muli
+        
+        String result = Encryption.replace("im ironman");
+        assertEquals("1m 1r0m@n",result);
+        
+    }
+        @Test
+    public void testAdd15(){//muli
+        
+        String result = Encryption.replace("David");
+        assertEquals("D@v1d",result);
+        
+    }
+    
 }
